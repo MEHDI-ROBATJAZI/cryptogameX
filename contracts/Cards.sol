ca0b7aaaef768171b4a5d63c1afdea42b1732c8c
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-08-25 12:17:36 +0200

    card struct adde

---
+++ contracts/Cards.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+contract Card {
+    // The ID of the parents of this card, set to 0 for gen0 cats.
+    // Note that using 32-bit unsigned integers limits us to a "mere"
+    // 4 billion cats. This number might seem small until you realize
+    // that Ethereum currently has a limit of about 500 million
+    // transactions per year! So, this definitely won't be a problem
+    // for several years (even as Ethereum learns to scale).
+    uint32 cardId;
+
+    // the power
+    uint32 power;
+
+    string name;
+
+    // The "generation number" of this cat. Cats minted by the CK contract
+    // for sale are called "gen0" and have a generation number of 0. The
+    // generation number of all other cats is the larger of the two generation
+    // numbers of their parents, plus one.
+    // (i.e. max(matron.generation, sire.generation) + 1)
+    uint16 generation;
+
+    // Do we need it? The card's genetic code is packed into these 256-bits, the format is
+    // sooper-sekret! A cat's genes never change.
+    uint256 genes;
+}
